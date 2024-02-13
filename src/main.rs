7ab24f33c4fb012c087c0af2ec31c221f4f994ad
Ian Klatzco <iklatzco@gmail.com>
2024-02-13 21:48:58 +0100

    test, exercise1 complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+// welcome!
+// the println!() "function" is your friend!
+// write println!() and pass in "Hallo, xhain!" 
+fn string_to_print() -> String {
+    return String::from("TODO");
+} 
+
 fn main() {
-    println!("Hello, xhain!");
+    let s = string_to_print();
+    println!("{}", s);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::string_to_print;
+
+    #[test]
+    fn it_works() {
+
+        assert_eq!(
+            string_to_print(), 
+            String::from("Hallo, xhain!")
+        );
+    }
 }
